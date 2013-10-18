6db6df5e83a0e13480f2a3ea59017ab0985ec3c7
Dustin Wilhelmi <dustin.wilhelmi@gmail.com>
2013-10-18 14:06:09 -0500

    Restore end level fireworks
    Fixes a couple of issues that were preventing the fireworks from appearing at the end of the level

--- things.js
+++ things.js
@@ -2567,7 +2567,7 @@ function endLevelPoints(me, detector) {
   // Determine the number of fireballs (1, 3, and 6 become not 0)
   var numfire = getLast(String(data.time.amount));
   if(!(numfire == 1 || numfire == 3 || numfire == 6)) numfire = 0;
-  
+
   // Count down the points (x50)
   var points = setInterval(function() {
     // 50 for each
@@ -2592,7 +2592,7 @@ function endLevelFireworks(me, numfire, detector) {
     // var castlemid = detector.castle.left + detector.castle.width * unitsized2;
     var castlemid = detector.left + 32 * unitsized2;
     while(i < numfire)
-      explodeFirework(i++, castlemid);
+      explodeFirework(++i, castlemid); //pre-increment since explodeFirework expects numbers starting at 1
     nextnum = timer * (i + 2) * 42;
   }
   else nextnum = 0;
@@ -2608,7 +2608,6 @@ function endLevelFireworks(me, numfire, detector) {
 }
 function explodeFirework(num, castlemid) {
   setTimeout(function() {
-    if(!map.ending) return;
     var fire = new Thing(Firework, num);
     addThing(fire, castlemid + fire.locs[0] - unitsize * 6, unitsizet16 + fire.locs[1]);
     fire.animate();
